6d5233e2a35da836f755f909a523d78e41fca677
Umer Mehmood <deanbusiness007@gmail.com>
2024-11-19 16:42:15 +0500

    incomplete

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ var usernameData = false;
 function inputValid(e) {
   if (e.target.value.length < 3) {
     console.log(e.target.nextElementSibling);
-    e.target.nextElementSibling.innerText = "atleast 3 char required";
+    e.target.nextElementSibling.innerText = "atleast 3 character required";
     e.target.nextElementSibling.style.display = "block";
     return;
   }
   e.target.nextElementSibling.style.display = "none";
   usernameData = true;
 }
+
 function submitData(e) {
   e.preventDefault();
   var username = document.getElementById("username");
   var password = document.getElementById("password");
   if (password.value.length < 11) {
-    password.nextElementSibling.innerText = "wrong password";
+    password.nextElementSibling.innerText = "Roll Number doesn't exist";
     return;
   }
   password.nextElementSibling.style.display = "none";
@@ -35,29 +36,26 @@ function submitData(e) {
       password: password.value,
     },
   ];
-  console.log(usersData);
+
   username.value = "";
   password.value = "";
   usernameData = false;
 }
 var usersData = [
   {
-    username: "abc1",
-    password: "1234567890",
+    username: "umercrusher55",
+    password: "55667799",
   },
   {
-    username: "abc2",
-    password: "1234567890354",
+    username: "razausman",
+    password: "52kk341",
   },
 ];
 
-let slideIndex = 0;
-showSlides();
-
+var slideIndex = 0;
 function showSlides() {
-  let i;
   let slides = document.getElementsByClassName("cd-img-code");
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
@@ -65,5 +63,7 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 10000);
+  setTimeout(showSlides, 5000);
 }
+showSlides();
+
